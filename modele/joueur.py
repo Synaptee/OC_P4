67f7771ab5158a9055bd156378f1c02dd3417d7a
Synaptee <jba.munoz@gmail.com>
2023-05-21 19:06:32 +0200

    Check joueur existant

--- modele/joueur.py
+++ modele/joueur.py
@@ -1,4 +1,4 @@
-from tinydb import TinyDB
+from tinydb import TinyDB,Query
 from pathlib2 import Path
 
 
@@ -6,7 +6,7 @@ base_dir = Path(__file__).resolve().parent.parent
 db_path = db_path = base_dir / 'database' / 'chess_mgt.json'
 
 class Joueur:
-    def __init__(self,nom, prenom, date_de_naissance, id_national):
+    def __init__(self, nom, prenom, date_de_naissance, id_national):
         self.nom = nom
         self.prenom = prenom
         self.date_de_naissance = date_de_naissance
@@ -16,8 +16,21 @@ class Joueur:
     def ajouter_joueur(nom, prenom, date_de_naissance, id_national):
         db = TinyDB(db_path)
         table = db.table('_default')
-        table.insert({'Nom': nom, 'Prénom': prenom, 'Date de naissance': date_de_naissance, 'ID': id_national})
-        print("Joueur ajouté à la base de données")
+        if not Joueur.verification_existence_joueur(id_national):
+            table.insert({'Nom': nom, 'Prénom': prenom, 'Date de naissance': date_de_naissance, 'ID': id_national})
+            print("Joueur ajouté à la base de données")
+
+    @staticmethod
+    def verification_existence_joueur(ID):
+        db = TinyDB(db_path)
+        table = db.table('_default')
+        query = table.search((Query().ID == ID))
+        if query:
+            print("\n Le joueur existe déjà dans la base de données \n")
+            return True
+        else:
+            print("Le joueur n'existe pas dans la base de données")
+            return False
 
 
 
