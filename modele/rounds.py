dd8f5da79419c56d2e3e7d991e040ecd973d15a3
Synaptee <jba.munoz@gmail.com>
2023-05-23 18:39:06 +0200

    Création Modeèle Round

---
+++ modele/rounds.py
@@ -0,0 +1,11 @@
+
+
+
+class Round:
+    def __init__(self, name, start_date, end_date):
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.matchs = []
+    
+    
--- test.py
+++ test.py
@@ -1,10 +1,52 @@
-from tinydb import TinyDB, Query
+import random
+from tinydb import TinyDB
 
+def create_random_player_list(num_players):
+    db = TinyDB('/Users/jbstudio/Desktop/DA_Python/OC_P4/database/joueurs.json')
+    players = db.table('_default')
+    player_data = players.all()
 
-def test():
-        db = TinyDB('/Users/jbstudio/Desktop/DA_Python/OC_P4/database/tournois.json')
-        table = db.table('_default')
-        query = table.search((Query().ID == "BLP2223"))
-        print(query[0]['Nom'])
+    # Vérification si le nombre demandé est supérieur au nombre total de joueurs disponibles
+    if num_players > len(player_data):
+        raise ValueError("Le nombre de joueurs demandé dépasse le nombre total de joueurs disponibles.")
 
-test()
+    # Sélection aléatoire des joueurs
+    selected_players = random.sample(player_data, num_players)
+
+    # Création de la liste de noms et prénoms des joueurs sélectionnés
+    player_list = []
+    for player in selected_players:
+        nom_prenom = f"{player['Nom']} {player['Prénom']}"
+        player_list.append(nom_prenom)
+
+    return player_list
+
+# Exemple d'utilisation
+num_joueurs_demandes = int(input("Combien de joueurs voulez-vous ? "))
+liste_joueurs = create_random_player_list(num_joueurs_demandes)
+print(liste_joueurs)
+
+def generate_random_matches(player_list):
+    num_players = len(player_list)
+    
+    # Vérification si le nombre de joueurs est impair
+    if num_players % 2 != 0:
+        raise ValueError("Le nombre de joueurs doit être pair pour générer des matchs.")
+    
+    random.shuffle(player_list)  # Mélange aléatoire des joueurs
+    
+    matches = {}
+    round_name = "round 1"
+    matches[round_name] = []
+    
+    # Création des paires de joueurs
+    for i in range(0, num_players, 2):
+        match = (player_list[i], player_list[i+1])
+        matches[round_name].append(match)
+    
+    return matches
+
+# Exemple d'utilisation
+print("***")
+matchs = generate_random_matches(liste_joueurs)
+print(matchs)
